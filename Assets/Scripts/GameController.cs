b06cccea9301afda59bfa0b97aaf10a86f4c1bef
lubmir2k <lubomir.mitana@gmail.com>
2021-03-04 11:42:38 +0100

    First commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject ballPrefab;
+	public Text score1Text;
+	public Text score2Text;
+	public float scoreCoordinates = 3.4f;
+
+	private Ball currentBall;
+	private int score1 = 0;
+	private int score2 = 0;
+
+	void Start()
+	{
+		SpawnBall();
+	}
+
+	void SpawnBall()
+	{
+		GameObject ballInstance = Instantiate(ballPrefab, transform);
+
+		currentBall = ballInstance.GetComponent<Ball>();
+		currentBall.transform.position = Vector3.zero;
+
+		score1Text.text = score1.ToString();
+		score2Text.text = score2.ToString();
+	}
+
+	void Update()
+	{
+		if (currentBall != null)
+		{
+			if (currentBall.transform.position.x > scoreCoordinates)
+			{
+				score1 += 1;
+				Destroy(currentBall.gameObject);
+				SpawnBall();
+			}
+
+			if (currentBall.transform.position.x < -scoreCoordinates)
+			{
+				score2 += 1;
+				Destroy(currentBall.gameObject);
+				SpawnBall();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed = 7f;
+
+    private void Update()
+    {
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(
+            0,
+            verticalMovement * speed
+        );
+    }
+}
